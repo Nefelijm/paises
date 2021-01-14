2d3b3b8c7bf1f17276d415532df43c8017a5eb5b
Nefelijm <sistemasnik20@gmail.com>
2021-01-14 12:03:13 -0500

    mostrando la lista de paises

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
+//Mostrando la lista de paises//
+const element = document.getElementById('countries');
+
+function createCountry(country) {
+
+    let div = document.createElement('div');
+    div.classList.add('col-4')
+    div.classList.add('colum')
+    div.innerHTML= 
+    `<p><a href="#">${country.name}</a></p>
+    <img class="image" src='${country.flag}'/>
+    ` 
+    element.appendChild(div)
+    
+}
+
 //Consumiendo el Servicio//
 const url = 'https://restcountries.eu/rest/v2/lang/es';
 fetch(url)
 .then(response => response.json())
 .then(data=>{
-    console.log(data)
+    console.log(data);
+
+    data.forEach(element => {
+        createCountry(element)
+    });
 })
 .catch(err=>console.log(err))
